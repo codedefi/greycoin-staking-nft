f685b98960c4ac0845ef3b0e4622ef0a0b9a05b3
XenosGrey <95102476+codedefi@users.noreply.github.com>
2024-08-09 15:52:17 -0500

    Initial commit

---
+++ const/contractAddresses.ts
@@ -0,0 +1,30 @@
+/** Replace the values below with the addresses of your smart contracts. */
+
+// 1. Set up the network your smart contracts are deployed to.
+// First, import the chain from the package, then set the NETWORK variable to the chain.
+import { MordorTestnet} from "@thirdweb-dev/chains";
+export const NETWORK = MordorTestnet;
+
+export const ETHERSCAN_URL = "https://etc-mordor.blockscout.com";
+
+// 2. The address of the marketplace V3 smart contract.
+// Deploy your own: https://thirdweb.com/thirdweb.eth/MarketplaceV3
+export const MARKETPLACE_ADDRESS = "";
+export const NFT_ADDRESS = "0xD7BfC3360138C78F7c286C755964c45BB35cA6a0";
+export const REWARDSADDRESS = "";
+export const stakingContractAddress = "0x9E30F684E03774D0380aa6c6A8151e4789b98a6d";
+export const tokenContractAddress = "";
+
+//3. Descriptions
+export const title = "Mink NFT Collection";
+export const description = ``;
+export const welcome = "Welcome to Mink NFTs";
+export const maxSupply = 50;
+export const mintPrice = 0.2;
+export const projectNfts = "NFTs";
+export const maxPerWallet = 5;
+
+//4. Social Media
+export const twitterLink = "https://twitter.com/";
+export const discordLink = ""
+export const telegramLink = ""
